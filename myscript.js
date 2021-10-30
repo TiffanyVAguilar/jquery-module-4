201774eee79dec1b7cde579bb578c3911bc2e1d5
TiffanyVAguilar <70239467+TiffanyVAguilar@users.noreply.github.com>
2021-10-30 02:56:38 -0500

    add jquery and do challenge 1

---
+++ myscript.js
@@ -0,0 +1,14 @@
+$(function () {
+  $(".product-list h2.product-name[data-type='mineralwater']").css(
+    "background-color",
+    "#8D3120"
+  );
+  $(".product-list h2.product-name[data-type='vitamin']").css(
+    "background-color",
+    "#1C9347"
+  );
+  $(".product-list h2.product-name[data-type='proteinbar']").css(
+    "background-color",
+    "#544D54"
+  );
+});
